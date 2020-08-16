fb73fcbdb57b8eacd3f120e8198250e813789244
Shaid Azmin <shaidazmin@gmail.com>
2020-08-16 19:34:29 +0600

    learn dart anonymus function & lamda expression

--- HelloDart.dart
+++ HelloDart.dart
@@ -1,18 +1,46 @@
 void main (){
   print ("Hello Dart Coder!");
-  var re = sum("Nahid", " Islam");
-  print(re);
 
-  Hello h = new Hello(11, 231, "Shaid Azmin");
+//
+//  var a = "u\128143";
+//  var ab = '\u1243';
+//  print(ab);
+//
+//  var re = sum("Nahid", " Islam");
+//  print(re);
+//
+//  Hello h = new Hello(11, 231, "Shaid Azmin");
+//
+//  print(h.toString());
+//
+//  for (int i = 0; i<= 5; i++){
+//    if ( i == 3 ){
+//      continue;
+//    }
+//    print(i);
+//  }
 
-  print(h.toString());
 
-  for (int i = 0; i<= 5; i++){
-    if ( i == 3 ){
-      continue;
-    }
+  var i = [1,2,3,5,7,8,987,7,89,45,55];
+  var ii = [1,2,3,5,7,8,987,7,89,45,55];
+  // lambda
+  i.forEach( (j) => print("${i.indexOf(j)}  value of $j"));
+
+  //anonymous function.....
+  i.forEach((j)
+  {
+    print("${i.indexOf(j)}  value of $j");
     print(i);
-  }
+    if(j==2){
+      if (i.contains(ii)) {
+        // andother array or list
+        print(ii);
+      }
+    }
+
+
+  });
+
 
 
 }
